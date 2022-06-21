4169a4c57b2a4bb2304aaed3cb47c44c2bb452e9
yonkov <yonkov.atanas@gmail.com>
2022-06-21 15:56:20 +0300

    icon fixed

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -35,6 +35,7 @@ const config: GatsbyConfig = {
          background_color: `#663399`,
          theme_color: `#663399`,
          display: `minimal-ui`,
+         icon: `src/images/icon.png`, // This path is relative to the root of the site.
        },
      },
   ]
